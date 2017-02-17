66303ed6aa2e8550ce2cdc531b69c539fa196ff8
Nicholas Rance <nicholas.rance@gmail.com>
2017-02-17 01:00:38 -0500

    changed schema

--- api.py
+++ api.py
@@ -24,17 +24,35 @@ api.route = types.MethodType(api_route, api)
 @api.route('/matches')
 class Server(Resource):
     def get(self):
-        output_dict = {}
+        output_dict = generate_matches()
         #number = request.args.get('number')
-        with open('bigdump2.json') as data_file:
-            data = json.load(data_file)
-        #for iter in range(0,int(number)):
-        for iter in range(0, 10):
-            output_dict[iter] = data[iter]
         return output_dict
         #print(data,file=sys.stderr)
 
+def generate_matches():
+    output_dict = {}
+    with open('bigdump2.json') as data_file:
+        data = json.load(data_file)
+    # for iter in range(0,int(number)):
+   # print(data[0])
+    for iter in range(0, 10):
+        output_dict[iter] = getMatchFields(data[iter][0])
+    return output_dict
 
+def getMatchFields(listing):
+    """
+    :param listing: Accepts the a listing and returns a list with only the fields needed to display a match
+    :return:
+    """
+    #print(listing)
+    clean_record = {}
+    clean_record["street_address"] = listing["address"]["street_address"]["label"]
+    clean_record["postal_code"] = listing["address"]["postal_code"]["value"]
+    clean_record["state"] = listing["address"]["region"]["label"]
+    clean_record["main_uri"] = listing["main_uri"]
+    clean_record["price"] = listing["price"]["value"]
+    clean_record["primary_image "] = listing["primary_image"]["src"]
+    return clean_record
 #api.add_resource(Server, '/')
 
 if __name__ == '__main__':
